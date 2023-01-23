b881ee9e404f0039cf1763a03005da5d838ea899
Gustavo Paseto Zonta <gustavozontauk@gmail.com>
2023-01-23 22:21:00 +0000

    :tada: Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle('light'); // troca do modo dark para o light e vice-versa
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // Substituir a imagem
+  if(html.classList.contains('light')) {
+    // se tiver o light-mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    // se tivert no dark-mode, adicionar a imagem dark
+    img.setAttribute('src', './assets/avatar.png')
+  }
+
+
+}
